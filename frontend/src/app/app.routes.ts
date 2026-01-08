a183251677e5b9b3dd2408f4b2865da5faa5e238
Hemant Giri <hg3776574@gmail.com>
2026-01-09 01:34:44 +0530

    Fix hash routing redirect issue

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,13 +2,5 @@ import { Routes } from '@angular/router';
 import { Notes } from './pages/notes/notes';
 
 export const routes: Routes = [
-    { 
-        path: '',
-        redirectTo: 'notes',
-        pathMatch: 'full' 
-    },
-    { 
-        path: 'notes',
-        component: Notes
-    }
+    { path: '', component: Notes }
 ];
